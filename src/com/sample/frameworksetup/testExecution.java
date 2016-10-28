93f905c272abc0df1098621ab42d828820a25f61
spk19 <spk19@192.168.1.114>
2016-10-28 07:52:42 -0400

    updated code.

---
+++ src/com/sample/frameworksetup/testExecution.java
@@ -0,0 +1,11 @@
+package com.sample.frameworksetup;
+
+import java.io.IOException;
+
+public class testExecution
+{	
+	public boolean executeTestCase(String testCaseNo, String testCaseDescription, String testCaseRunSel, String testCaseBrowser)
+	{
+		return true;
+	}
+}
